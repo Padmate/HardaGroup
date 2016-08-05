a2adc6ff7bce3f892e3e29df9445934a647eadab
Padmate <959853840@qq.com>
2016-08-05 18:07:17 +0800

    国际化

--- HardaGroup.Web/App_Start/RouteConfig.cs
+++ HardaGroup.Web/App_Start/RouteConfig.cs
@@ -13,6 +13,14 @@ namespace HardaGroup.Web
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            //全球化路由
+            routes.MapRoute(
+                "Globalization", // 路由名称
+                "{language}/{controller}/{action}/{id}", // 带有参数的 URL
+                new { language = "zh", controller = "Home", action = "Default", id = UrlParameter.Optional }, // 参数默认值
+                new { language = "^[a-zA-Z]{2}(-[a-zA-Z]{2})?$" }    //参数约束
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
